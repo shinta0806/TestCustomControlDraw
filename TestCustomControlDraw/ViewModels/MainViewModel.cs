e6566ba7bd23f35a878719c97b32514e40bc6582
shinta0806 <shinta.0806@gmail.com>
2023-05-05 17:19:11 +0900

    プロジェクト ファイルを追加します。

---
+++ TestCustomControlDraw/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TestCustomControlDraw.ViewModels;
+
+public class MainViewModel : ObservableRecipient
+{
+    public MainViewModel()
+    {
+    }
+}
---
+++ TestCustomControlDraw/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+using TestCustomControlDraw.ViewModels;
+
+namespace TestCustomControlDraw.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainViewModel ViewModel
+    {
+        get;
+    }
+
+    public MainPage()
+    {
+        ViewModel = App.GetService<MainViewModel>();
+        InitializeComponent();
+    }
+}
